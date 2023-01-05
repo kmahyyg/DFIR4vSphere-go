d292a480fcdd92df78e80d03fc816d50ba5fd833
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2023-01-06 02:27:56 +0800

    build(script): update version string embed

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,3 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/../
 rm -rf bin/*
 mkdir -p bin/
+rm -f pkg/common/gitversion.txt
+export PROG_VERSION=$(git describe --long --tags --always --dirty)
+# do not include any line break characters, use -n, they are illegal in http headers
+echo -n "${PROG_VERSION}" > ./pkg/common/gitversion.txt
