4f9b72132a6347bcdf06a616d87a452724dcb6b7
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2023-01-26 11:00:58 +0800

    build: scripts update

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,6 +5,4 @@ cd ${SCRIPT_DIR}/../
 rm -rf bin/*
 mkdir -p bin/
 rm -f pkg/common/gitversion.txt
-export PROG_VERSION=$(git describe --long --tags --always --dirty)
-# do not include any line break characters, use -n, they are illegal in http headers
-echo -n "${PROG_VERSION}" > ./pkg/common/gitversion.txt
+. ${SCRIPT_DIR}/versioning.sh
